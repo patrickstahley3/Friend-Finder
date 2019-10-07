af58dda09192df5851bebab82530a4742023d791
patrickstahley3 <patrickstahley3@gmail.com>
2019-10-06 20:07:01 -0700

    updated paths

--- routing/htmlroutes.js
+++ routing/htmlroutes.js
@@ -2,11 +2,11 @@ var path = require('path');
 
 module.exports = function(app) {
  
-    app.get("./routing/home", function (req, res) {
+    app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "./../Public/home.html"));
-    });   
+    }); 
     
-    app.get("./routing/survey", function (req, res) {
+    app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "./../Public/survey.html"));
     });
 }
